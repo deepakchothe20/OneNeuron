890fe489f9fbb6247586cbc9dcb5b6fe924332be
DeepakChothe <deepakchothe20@gmail.com>
2024-05-20 20:03:49 +0530

    first commit

---
+++ and.py
@@ -0,0 +1,23 @@
+from utility.model import Perceptron
+import pandas as pd
+from utility.utility import prepare_data
+
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,0,0,1],
+}
+
+df = pd.DataFrame(AND)
+
+df
+
+X,y = prepare_data(df)
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+model.fit(X, y)
+
+_ = model.total_loss()
